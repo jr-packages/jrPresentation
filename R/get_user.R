906b0dc83833f2bf7964efe673103352a04027fb
Jamie <jamie@jumpingrivers.com>
2018-10-26 16:08:10 +0100

    add updated docs

--- R/get_user.R
+++ R/get_user.R
@@ -3,7 +3,7 @@
 #' Returns a nicely formatted name and twitter handle.
 #' @param user By default grabs the system name and matches user.
 #' @examples
-#' get_author(user = "ncsg3")
+#' get_user(user = "ncsg3")
 #' @export
 get_user = function (user = NULL) {
   if (is.null(user))
