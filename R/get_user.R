fec8d21f2acdcd113569aac478942cd8cedce023
Jamie <jamie@jumpingrivers.com>
2018-10-26 15:52:24 +0100

    add a get_user function for pushing to connect

---
+++ R/get_user.R
@@ -0,0 +1,18 @@
+#' Presentation user_name for rstudio connect
+#'
+#' Returns a nicely formatted name and twitter handle.
+#' @param user By default grabs the system name and matches user.
+#' @examples
+#' get_author(user = "ncsg3")
+#' @export
+get_author = function (user = NULL) {
+  if (is.null(user))
+    user = Sys.info()["user"]
+  if (user %in% c("ncsg3", "colin")) {
+    "colin"
+  }
+  else if (user == "jamie") {
+    "u450" # TODO update this once seb back with my user on rstudio connect
+  }
+  # TODO add theo
+}
