7ad8c0fd62de1dbb73be01d3c0ea425f7b35220c
Jamie <jamie@jumpingrivers.com>
2019-01-31 08:54:00 +0000

    added some docs, added missing .Rd file which caused travis fail

--- R/create_feedback.R
+++ R/create_feedback.R
@@ -1,3 +1,7 @@
+#' @title Create a feedback slide
+#'
+#' @description Copies the feedback slide template to the current WD. This feedback slide will look for the config questionnaire item
+#'
 #' @export
 create_feedback = function(){
   feedback = system.file("feedback/feedback.Rmd",package="jrPresentation")
