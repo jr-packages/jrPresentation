397cf36cc29a56843451894c73d624dfdbf1b8af
Theo Roe <theo@jumpingrivers.com>
2019-08-07 15:51:59 +0100

    fixing get_connect_name

--- R/connect.R
+++ R/connect.R
@@ -74,7 +74,7 @@ get_connect_name = function() {
   on.exit(close(git_config))
   l = readLines(git_config)
   git_url = l[grep(pattern = "\turl", l) ]
-  pkg = stringr::str_match(git_url, "course_notes/(.*)/(.*)_notes.git")
+  pkg = stringr::str_match(git_url, "(jumpingrivers-notes|course_notes)/(.*)/(.*)_notes.git")
   pkg = pkg[, length(pkg)]
   pkg
 }
